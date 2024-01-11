21ea888125f66ce5bf62840721491137d3e6832d
Elena Mankova <liv_61@mail.ru>
2024-01-12 00:18:35 +0700

    added components Header, Navbar, Profile

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export const Header = () => {
+	return (
+		<header className="App-wrapper__header">
+			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsTSUyiVzgcEoi8IaiX4NOY51f-uy7I3rWhw&usqp=CAU" alt="logo" />
+	  </header>
+	)
+}
